7fbfc0a6eeff85b83e628decfebdd1d3a6f14709
Hannah Li <hannahli@berkeley.edu>
2015-09-16 13:57:01 -0700

    hey

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,15 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a)
+      a = a.map {|str| str.to_i}
+      a = a.map {|i| i + 2}
+      a.uniq
+      a.delete_if &:odd?
+      a.delete_if {|el| el > 10}
+      sum = 0
+      for el in a
+          sum += el
+      end
+ return sum
+      end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,18 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  return (a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  result = a.sort
+  result = result.map {|e| (e + 1)}
+  return result
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
